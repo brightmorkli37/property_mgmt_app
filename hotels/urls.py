1f6a97a692f2f7cac6e14aaee743035f9584a541
brightmorkli37 <brightmorkli37@gmail.com>
2022-05-09 16:03:33 +0000

    frontend skeleton

--- hotels/urls.py
+++ hotels/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'hotels'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.homepage, name='homepage'),
+    path('properties/', views.properties, name='properties'),
+    path('about-Us/', views.aboutUs, name='about'),
+    path('contact-Us/', views.contactUs, name='contact'),
 ]
--- root/urls.py
+++ root/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('', include('hotels.urls', namespace='hotels')),
 ]
 
-# urlpatterns += static(
-#         settings.STATIC_URL, document_root=settings.STATIC_ROOT
-#     )
+urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
 
 urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
